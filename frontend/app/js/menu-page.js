77b36f4bd74184dfa9cc9d887a5682eb6f7a1551
Chernovm <tchernovm@gmail.com>
2020-07-20 22:14:04 +0700

    Make ajax requests cancellable

--- frontend/app/js/menu-page.js
+++ frontend/app/js/menu-page.js
@@ -3,30 +3,43 @@ import PizzaCard from "./pizza-card";
 import errorHandler from "../error-handler";
 import BeverageCard from "./beverage-card";
 
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
+
 export default class MenuPage {
-    static loadPizzas() {
-        axios.get('/products?type=pizza')
-            .then(function (response) {
-                let pizzaContainer = document.getElementById('pizza-list');
-                pizzaContainer.innerHTML = '';
-                for (let i = 0; i < response.data.length; i++) {
-                    let card = new PizzaCard(response.data[i]);
-                    pizzaContainer.appendChild(card.getHtml());
-                }
-            })
-            .catch(errorHandler);
+    constructor() {
+        window.onbeforeunload = function() {
+            source.cancel('Operation canceled by the user.');
+        };
     }
 
-    static loadBeverages() {
-        axios.get('/products?type=beverage')
-            .then(function (response) {
-                let beverageContainer = document.getElementById('beverage-list');
-                beverageContainer.innerHTML = '';
-                for (let i = 0; i < response.data.length; i++) {
-                    let card = new BeverageCard(response.data[i]);
-                    beverageContainer.appendChild(card.getHtml());
-                }
-            })
-            .catch(errorHandler);
+    loadPizzas() {
+        axios.get('/products?type=pizza', {
+            cancelToken: source.token
+        })
+        .then(function (response) {
+            let pizzaContainer = document.getElementById('pizza-list');
+            pizzaContainer.innerHTML = '';
+            for (let i = 0; i < response.data.length; i++) {
+                let card = new PizzaCard(response.data[i]);
+                pizzaContainer.appendChild(card.getHtml());
+            }
+        })
+        .catch(errorHandler);
+    }
+
+    loadBeverages() {
+        axios.get('/products?type=beverage', {
+            cancelToken: source.token
+        })
+        .then(function (response) {
+            let beverageContainer = document.getElementById('beverage-list');
+            beverageContainer.innerHTML = '';
+            for (let i = 0; i < response.data.length; i++) {
+                let card = new BeverageCard(response.data[i]);
+                beverageContainer.appendChild(card.getHtml());
+            }
+        })
+        .catch(errorHandler);
     }
 }
