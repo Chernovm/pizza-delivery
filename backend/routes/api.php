922a93d9952ea36e6e089f0c984862acb7e2b306
Chernovm <tchernovm@gmail.com>
2020-07-20 22:15:22 +0700

    Implement backend for Orders

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('products', 'API\ProductController');
+Route::apiResource('orders', 'API\OrderController');
